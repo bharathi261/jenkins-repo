0aa7d00ecfcf6dfe8f0ca6db2b7c6f7187b44697
bharathi261 <bharathigowda11@gmail.com>
2025-06-30 23:09:32 +0530

    Update App.java file

--- src/main/java/com/google/App.java
+++ src/main/java/com/google/App.java
@@ -7,5 +7,6 @@ public class App {
     public static void main(String[] args) {
         System.out.println("Hello World!, Jenkins");
         System.out.println("Hi,Jenkins");
+        System.out.println(Helloall,Welcome to DevOps");
     }
 }
