d622bb358b8685552ea4bbcb68c51b95cd29e63c
bharathi261 <bharathigowda11@gmail.com>
2025-06-30 23:05:15 +0530

    Update App.java
    added contents in App.java

--- src/main/java/com/google/App.java
+++ src/main/java/com/google/App.java
@@ -6,7 +6,6 @@ package com.google;
 public class App {
     public static void main(String[] args) {
         System.out.println("Hello World!, Jenkins");
-        
-        
+        System.out.println("Hi,Jenkins");
     }
 }
